4b1309682b383dc05d0e1d3ff986afa45b9990bd
Val Melamed <valo@vtmelamed.com>
2026-06-22 10:09:06 -0400

    chore(devops): Refactor dependency management and clean up using directives
    - Removed transitive dependencies for Microsoft.Extensions.Configuration, Microsoft.Extensions.DependencyInjection, and Microsoft.Extensions.Logging from various package lock files.
    - Updated package references to use CentralTransitive for the aforementioned dependencies.
    - Cleaned up using directives in usings.cs files across the benchmarks, source, and test projects to remove unnecessary global usings.
    - Adjusted project references in Glob.Api.FakeFileSystem.csproj for consistency.
    - Added Microsoft.Extensions.TimeProvider.Testing to test projects for enhanced testing capabilities.

--- tests/Glob.Api.FakeFileSystem.Tests/usings.cs
+++ tests/Glob.Api.FakeFileSystem.Tests/usings.cs
@@ -1,14 +1,8 @@
 ﻿// SPDX-License-Identifier: MIT
 // Copyright (c) 2025-2026 Val Melamed
 
-global using System.Diagnostics.CodeAnalysis;
-global using System.Text.Json;
-
-global using Microsoft.Extensions.Configuration;
-
 global using Xunit.Sdk;
 
-global using vm2.TestUtilities;
 global using vm2.Glob.Api.FakeFileSystem;
 
 global using static vm2.TestUtilities.TestUtilities;
